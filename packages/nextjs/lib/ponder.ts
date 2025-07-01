c86af1dabad5babc45aae0a3724331204b581a7c
azf20 <azfuller20@gmail.com>
2025-07-01 15:39:45 +0100

    fix up urls

--- packages/nextjs/lib/ponder.ts
+++ packages/nextjs/lib/ponder.ts
@@ -1,6 +1,11 @@
 import * as schema from "./ponder.schema";
 import { createClient } from "@ponder/client";
 
-const client = createClient("http://localhost:42069/sql", { schema });
+
+const url = ["10", "11155111"].includes(process.env.NEXT_PUBLIC_CHAIN_ID || "31337")
+  ? `https://supersocks-${process.env.NEXT_PUBLIC_CHAIN_ID}.up.railway.app/sql`
+  : "http://localhost:42069/sql";
+
+const client = createClient(url, { schema });
 
 export { client, schema };
